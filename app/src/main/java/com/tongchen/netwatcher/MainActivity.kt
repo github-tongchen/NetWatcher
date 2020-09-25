5bc3e12b3ace909c3e138e57fd8a5d85bfb24f46
tongchen <839706407@qq.com>
2020-09-25 08:01:59 +0800

    完善框架

--- app/src/main/java/com/tongchen/netwatcher/MainActivity.kt
+++ app/src/main/java/com/tongchen/netwatcher/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import com.tongchen.componentservice.module.gank.GankService
 import com.tongchen.componentservice.module.mzitu.MZiTuService
 import com.tongchen.componentservice.router.ui.RouteManager
+import kotlinx.android.synthetic.main.app_activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,9 +19,9 @@ class MainActivity : AppCompatActivity() {
         mGankService = RouteManager.navigation(GankService::class.java)
         mMZiTuService = RouteManager.navigation(MZiTuService::class.java)
 
-        /*tv_name.setText(mGankService.getNameRes())
+        tv_title.setText(mGankService.getNameRes())
         val transaction = supportFragmentManager.beginTransaction()
-        transaction.add(R.id.app_fl_container, mGankService.getFragment())
-        transaction.commit()*/
+        transaction.add(R.id.fl_module_container, mGankService.getFragment())
+        transaction.commit()
     }
 }
