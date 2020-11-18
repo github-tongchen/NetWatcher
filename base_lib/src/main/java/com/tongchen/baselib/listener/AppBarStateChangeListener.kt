385e9c7486d2672b7daeb59ea586798a450183f3
tongchen <839706407@qq.com>
2020-11-18 18:46:40 +0800

    完成GankText的相关逻辑

---
+++ base_lib/src/main/java/com/tongchen/baselib/listener/AppBarStateChangeListener.kt
@@ -0,0 +1,48 @@
+package com.tongchen.baselib.listener
+
+import com.google.android.material.appbar.AppBarLayout
+import kotlin.math.abs
+
+/**
+ * @author TongChen
+ * @date 2019/11/22  10:50
+ * <p>
+ * Desc: 对CollapsingToolbarLayout 的 OnOffsetChangedListener 进行一次封装
+ */
+abstract class AppBarStateChangeListener : AppBarLayout.OnOffsetChangedListener {
+
+    enum class State {
+        EXPANDED,
+        COLLAPSED,
+        IDLE
+    }
+
+    private var mCurrentState = State.IDLE
+
+    override fun onOffsetChanged(appBarLayout: AppBarLayout, verticalOffset: Int) {
+        when {
+            (verticalOffset == 0) -> {
+                if (mCurrentState != State.EXPANDED) {
+                    onStateChanged(appBarLayout, State.EXPANDED)
+                }
+                mCurrentState = State.EXPANDED
+            }
+
+            (abs(verticalOffset) >= appBarLayout.totalScrollRange) -> {
+                if (mCurrentState != State.COLLAPSED) {
+                    onStateChanged(appBarLayout, State.COLLAPSED)
+                }
+                mCurrentState = State.COLLAPSED
+            }
+
+            else -> {
+                if (mCurrentState != State.IDLE) {
+                    onStateChanged(appBarLayout, State.IDLE)
+                }
+                mCurrentState = State.IDLE
+            }
+        }
+    }
+
+    abstract fun onStateChanged(appBarLayout: AppBarLayout, state: State)
+}
--- module_gank/src/main/java/com/tongchen/gank/biz/ui/fragment/ContentTextFragment.kt
+++ module_gank/src/main/java/com/tongchen/gank/biz/ui/fragment/ContentTextFragment.kt
@@ -1,11 +1,22 @@
 package com.tongchen.gank.biz.ui.fragment
 
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.webkit.WebChromeClient
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.annotation.RequiresApi
 import com.alibaba.android.arouter.facade.annotation.Route
+import com.bumptech.glide.Glide
+import com.google.android.material.appbar.AppBarLayout
+import com.tongchen.baselib.listener.AppBarStateChangeListener
 import com.tongchen.gank.R
 import com.tongchen.gank.base.GankBaseDBFragment
 import com.tongchen.gank.databinding.ModuleGankFragmentTextBinding
 import com.tongchen.gank.biz.entity.GankResult
+import kotlinx.android.synthetic.main.module_gank_fragment_text.*
 
 /**
  * @author TongChen
@@ -17,8 +28,10 @@ import com.tongchen.gank.biz.entity.GankResult
 @Route(path = "/gank/fragment")
 class ContentTextFragment : GankBaseDBFragment<ModuleGankFragmentTextBinding>() {
 
+    private var mGankResult: GankResult? = null
+
     companion object {
-        internal const val ARG_GANK_RESULT = "gank_result"
+        private const val ARG_GANK_RESULT = "gank_result"
 
         fun newInstance(result: GankResult): ContentTextFragment {
             val fragment = ContentTextFragment()
@@ -29,10 +42,96 @@ class ContentTextFragment : GankBaseDBFragment<ModuleGankFragmentTextBinding>() 
         }
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (arguments != null) {
+            mGankResult = arguments!!.getParcelable(ARG_GANK_RESULT)
+        }
+    }
+
     override fun inject2Fragment() {
         dataBindingComponent()?.inject(this)
     }
 
     override fun getLayoutId(): Int = R.layout.module_gank_fragment_text
 
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        val imgList = mGankResult?.images
+
+        if (imgList != null) {
+            if (imgList.isNotEmpty()) {
+                Glide.with(activity!!).load(imgList[0]).into(iv_head_bg)
+            }
+            appbarLyt.setExpanded(true)
+
+        } else {
+            appbarLyt.setExpanded(false)
+        }
+
+        appbarLyt.addOnOffsetChangedListener(object : AppBarStateChangeListener() {
+            override fun onStateChanged(appBarLayout: AppBarLayout, state: State) {
+                when (state) {
+                    State.EXPANDED, State.IDLE -> {
+                        mtv_title.text = ""
+                        collapsingToolbarLyt.title = mGankResult?.desc
+                        toolbar.navigationIcon = null
+                    }
+
+                    State.COLLAPSED -> {
+                        mtv_title.text = mGankResult?.desc
+                        collapsingToolbarLyt.title = ""
+                        toolbar.setNavigationIcon(R.drawable.ic_arrow_back)
+                    }
+                }
+            }
+        })
+
+        tv_publish_date.text = String.format(getString(R.string.module_gank_publish_date), mGankResult?.publishedAt!!.split("T")[0])
+        //  先隐藏加载完成后再显示
+        tv_publish_date.visibility = View.GONE
+
+        web_content.webViewClient = object : WebViewClient() {
+
+            override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+                web_content.loadUrl(url)
+                return true
+            }
+
+            @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+            override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
+                web_content.loadUrl(request?.url.toString())
+                return true
+            }
+        }
+
+        web_content.webChromeClient = object : WebChromeClient() {
+            override fun onProgressChanged(view: WebView?, newProgress: Int) {
+                super.onProgressChanged(view, newProgress)
+                if (progressbar_4_web == null) {
+                    return
+                }
+                //  进度条处理
+                if (progressbar_4_web.visibility == View.GONE) {
+                    progressbar_4_web.visibility = View.VISIBLE
+                }
+                progressbar_4_web.progress = newProgress
+                if (newProgress == 100) {
+                    progressbar_4_web.visibility = View.GONE
+                    tv_publish_date.visibility = View.VISIBLE
+                }
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        web_content.loadUrl(mGankResult?.url)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        web_content.stopLoading()
+    }
 }
